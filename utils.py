7194b0bc532857d048a87c4defaaa2b47df52927
EmelineToustou <emeline.toustou@outlook.com>
2019-05-25 14:00:08 +0200

    Fin exercice 1

--- utils.py
+++ utils.py
@@ -1,9 +1,13 @@
 def calcul_nb_voisins(Z):
-	"""Calcul du nombre de voisins vivants.
+	"""
+
+	Calcul du nombre de voisins vivants.
 
 	Entrée: Prend en argument une liste (de liste) qui 
         représente la "carte" du jeu de la vie.
-        Sortie: Renvoie le nombre de voisins vivants de chaque cellules"""
+    Sortie: Renvoie le nombre de voisins vivants de chaque cellule.
+
+    """
 
 	
 	forme = len(Z), len(Z[0])
@@ -20,17 +24,21 @@ def calcul_nb_voisins(Z):
 
 
 def iteration_jeu(Z):
-	"""Entrée : Prend en argument une liste de (listes) représentant l'état initial du jeu. 
+	"""
+
+	Entrée : Prend en argument une liste de (listes) représentant l'état initial du jeu. 
 	Sortie : Retourne une liste de listes correspondant à l'état du jeu à l'étape suivante .
 
-	Pour chaque cellule qui n'est pas sur le pourtour de la grille, 
+	Pour chaque cellule qui n'est pas sur le pourtour de la grille (considéré toujours mort), 
 	iteration_jeu détermine le nouvel état de la cellule (vivante ou morte)
-	en fonction de son nombre de voisins vivants.
+	en fonction de son nombre de voisins vivants, calculé par la fonction calcul_nb_voisins.
 
-	Une cellule morte avec trois voisins vivants exactement nait.
-	Une cellule vivante ayant deux ou trois voisins vivants reste en vie.
-	Une cellule vivante ayant au moins quatre voisins vivants meurt.
-	Une cellule vivante ayant zero ou un voisin vivant meurt."""
+	Une cellule morte avec trois voisins vivants exactement nait (naissance).
+	Une cellule vivante ayant deux ou trois voisins vivants reste en vie (équilibre).
+	Une cellule vivante ayant au moins quatre voisins vivants meurt par étouffement.
+	Une cellule vivante ayant zero ou un voisin vivant meurt par isolement.
+
+	"""
 
 	forme = len(Z), len(Z[0])
 	N = calcul_nb_voisins(Z)
@@ -49,7 +57,15 @@ from numba import jit
 
 @jit(nopython=True)
 def calcul_nb_voisins_jit(Z):
-	"""Calcul du nombre de voisins vivants."""
+	"""
+
+	Calcul du nombre de voisins vivants.
+
+	Entrée: Prend en argument une liste (de liste) qui 
+        représente la "carte" du jeu de la vie.
+    Sortie: Renvoie le nombre de voisins vivants de chaque cellule.
+
+	"""
 	forme = len(Z), len(Z[0])
 	N = [[0, ] * (forme[0]) for i in range(forme[1])]
 	for x in range(1, forme[0] - 1):
@@ -67,17 +83,21 @@ def calcul_nb_voisins_jit(Z):
 
 @jit(nopython=True)
 def iteration_jeu_jit(Z):
-	"""Entrée : Prend en argument une liste de (listes) représentant l'état initial du jeu. 
+	"""
+
+	Entrée : Prend en argument une liste de (listes) représentant l'état initial du jeu. 
 	Sortie : Retourne une liste de listes correspondant à l'état du jeu à l'étape suivante .
 
-	Pour chaque cellule qui n'est pas sur le pourtour de la grille, 
+	Pour chaque cellule qui n'est pas sur le pourtour de la grille (considéré toujours mort), 
 	iteration_jeu détermine le nouvel état de la cellule (vivante ou morte)
-	en fonction de son nombre de voisins vivants.
+	en fonction de son nombre de voisins vivants, calculé par la fonction calcul_nb_voisins.
 
-	Une cellule morte avec trois voisins vivants exactement nait.
-	Une cellule vivante ayant deux ou trois voisins vivants reste en vie.
-	Une cellule vivante ayant au moins quatre voisins vivants meurt.
-	Une cellule vivante ayant zero ou un voisin vivant meurt."""
+	Une cellule morte avec trois voisins vivants exactement nait (naissance).
+	Une cellule vivante ayant deux ou trois voisins vivants reste en vie (équilibre).
+	Une cellule vivante ayant au moins quatre voisins vivants meurt par étouffement.
+	Une cellule vivante ayant zero ou un voisin vivant meurt par isolement.
+
+	"""
 
 	forme = len(Z), len(Z[0])
 	N = calcul_nb_voisins_jit(Z)
@@ -88,3 +108,30 @@ def iteration_jeu_jit(Z):
 			elif Z[x][y] == 0 and N[x][y] == 3:
 				Z[x][y] = 1
 	return Z
+
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+def affichage_jeu(Etat_jeu, Iteration = 0):
+	"""
+
+	Entrée : Prend en arguments un np.array représentant l'état du jeu de la vie
+			et un nombre d'itérations du jeu à réaliser.
+
+	affichage_jeu effectue le nombre d'itérations du jeu de la vie
+	à partir de l'état renseigné et affiche l'état obtenu du jeu.
+
+	"""
+
+	Z_iter = np.copy(Etat_jeu)
+	plt.figure(figsize = (10, 5))
+
+	for i in range(Iteration):
+		Z_iter = iteration_jeu(Z_iter)
+
+	plt.imshow(Z_iter)
+	plt.title("Etape " + str(Iteration) + " du jeu de la vie")
